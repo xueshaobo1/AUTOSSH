c721580d9112ef893d34fef481cd6a8d61a2b145
xueshaobo <522336309@qq.com>
2018-11-02 06:34:36 -0400

    自动添加脚本

--- timedtask/urls.py
+++ timedtask/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 urlpatterns = [
-
+	url(r'^shell/', views.shell),
+	url(r'^add_shell/', views.add_shell),
+	url(r'^del_shell/', views.del_shell),
+	url(r'^task/', views.task),
 ]
 
--- timedtask/views.py
+++ timedtask/views.py
@@ -2,5 +2,34 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from . import models
+import os
 
-# Create your views here.
+
+def shell(request):
+	s = os.path.join((os.getcwd()),'shell')
+	shell_list = os.listdir(s)
+	return render(request, 'shell.html',locals())
+
+def add_shell(request):
+	if request.method == "POST":
+		shell_name = request.POST.get('name')
+		shell_body = request.POST.get('body')
+		pwd = os.path.join((os.getcwd()),'shell',shell_name)
+		f = open(pwd,'w+')
+		p = f.write(shell_body)
+		f.close()
+		return redirect('/timedtask/shell/')
+
+def del_shell(request):
+	if request.method == "POST":
+		shell_name = request.POST.get('name')
+		os.remove(os.path.join((os.getcwd()),'shell',shell_name))
+		return redirect('/timedtask/shell/')
+
+def task(request):
+	task_name = os.system("crontab -l")
+	print task_name
+	return render(request, 'task.html',locals())
